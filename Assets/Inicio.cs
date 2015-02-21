6a1ef7af24c56be9179bb71f5678c93a0a682a73
Yhozen <gabriel@garox.org>
2015-02-21 18:07:36 -0300

    0.0.2.2
    Agregado menu de inicio

---
+++ Assets/Inicio.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inicio : MonoBehaviour {
+
+
+	// Update is called once per frame
+	public void ChangeScene (int sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
