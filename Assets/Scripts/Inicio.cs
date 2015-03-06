112d01dc201a157852b09ddfd9216afd8991529e
Yhozen <gabriel@garox.org>
2015-03-05 21:58:01 -0300

    0.0.3.3
    Ordenado, trabajando en DoF

---
+++ Assets/Scripts/Inicio.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inicio : MonoBehaviour {
+
+	public void Salir(bool PressExit){
+		if (PressExit) {
+			Application.Quit();
+		}
+	}
+	// Update is called once per frame
+	public void ChangeScene (int sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+	    public void Update () {
+	   if (Input.GetKey ("escape")) {
+		   ChangeScene(2);
+	}
+}
+}
---
+++ Assets/Scripts/Opciones.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Opciones : MonoBehaviour {
+	public string Nombre;
+	public void SetNombre(bool PlayerNombre) {
+		if  (PlayerNombre) {
+			var Nombre = "Yhozen";
+		}
+	}
+
+//	public string PlayerName(string Nombre){
+//	 string Evercito = Nombre;
+//			return Evercito ;
+//
+//	}
+
+}
