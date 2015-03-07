a948a6fd5e0ee49bd37cb0122e8421a7d9d3ba6e
Yhozen <gabriel@garox.org>
2015-03-07 12:13:15 -0300

    0.0.3.4
    Eliminado Texturas sobrantes, trabajando escena de opciones

---
+++ Assets/Scripts/AudioSlider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSlider : MonoBehaviour {
+ public float VolumenCancion = 1F;
+public  void Examplo(float Sonido) {
+        Sonido = AudioListener.volume;
+    }
+}
