b95df5e5c20a401a542147ee9a327ed289d42871
Yhozen <gabriel@garox.org>
2015-03-09 19:51:18 -0300

    Revert "0.0.3.5.2"
    This reverts commit 17f7d495c84458f0dbb25521ce2412ed0ae1a905.

---
+++ Assets/Scripts/AudioSlider.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class AudioSlider : MonoBehaviour {
+	public Text  charText;
+	// Use this for initialization
+	void Start () {
+	charText = GameObject.Find("CharText").GetComponent<Text>();
+	}
+
+public void CharacterField(string inputFieldString) {
+	charText.text = inputFieldString;
+}
+}
