f1ff39f74b8469d01351a309d81277ed4549a95f
Gabriel <gabriel@garox.org>
2018-01-06 01:02:01 -0300

    style

--- Assets/Scripts/Inicio.cs
+++ Assets/Scripts/Inicio.cs
@@ -3,16 +3,14 @@ using System.Collections;
 
 public class Inicio : MonoBehaviour {
 
-	public void Salir(bool PressExit){
-		if (PressExit) {
-			Application.Quit();
-		}
+	public void Salir (bool PressExit) {
+		if (PressExit) Application.Quit();
 	}
-	// Update is called once per frame
+
 	public void ChangeScene (int sceneToChangeTo) {
 		Application.LoadLevel (sceneToChangeTo);
 	}
-	    public void Update () {
+    public void Update () {
 	   if (Input.GetKey ("escape")) {
 		   ChangeScene(2);
 	}
--- Assets/Scripts/InputNameField.cs
+++ Assets/Scripts/InputNameField.cs
@@ -1,19 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
 public class InputNameField : MonoBehaviour {
 	public Text  charText;
 	// Use this for initialization
 	void Start () {
-//	charText = GameObject.Find("CharText").GetComponent<Text>();
 	}
 
-public void CharacterField(string inputFieldString) {
-	charText.text = inputFieldString;
-}
-public void ConfirmEver() {
-  if(charText.text.Length >= 2) {
-    PlayerPrefs.SetString("charName", charText.text);
-  }
-}
+	public void CharacterField(string inputFieldString) {
+		charText.text = inputFieldString;
+	}
+	public void ConfirmEver() {
+	  if(charText.text.Length >= 2) PlayerPrefs.SetString("charName", charText.text);
+	}
 }
